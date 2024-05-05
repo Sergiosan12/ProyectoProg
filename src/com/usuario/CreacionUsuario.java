187a33006dde55a819edd33cf674155218d9d2ef
Sergio <ssanromangiraldez@danielcastelao.org>
2024-05-05 16:07:52 +0200

    Getters y setters de la clase Usuario

--- src/com/usuario/CreacionUsuario.java
+++ src/com/usuario/CreacionUsuario.java
@@ -1,4 +1,13 @@
 package com.usuario;
 
 public class CreacionUsuario {
+    private Usuario usuario;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
--- src/com/usuario/Usuario.java
+++ src/com/usuario/Usuario.java
@@ -1,4 +1,59 @@
 package com.usuario;
 
 public class Usuario {
+    private int id;
+    private String nombre;
+    private String email;
+    private String contrasena;
+    private int edad;
+
+    public Usuario(int id, String nombre, String email, String contrasena, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.contrasena = contrasena;
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+
 }
