5bd929f2d1fdcd512cf829f5fc6b219039fe2e24
Sergio <ssanromangiraldez@danielcastelao.org>
2024-05-05 15:58:58 +0200

    Getters y setters de las fases

--- src/com/fases/FaseLutea.java
+++ src/com/fases/FaseLutea.java
@@ -1,4 +1,13 @@
 package com.fases;
 
 public class FaseLutea {
+    float mediaDiasLutea;
+
+    public float getMediaDiasLutea() {
+        return mediaDiasLutea;
+    }
+
+    public void setMediaDiasLutea(float mediaDiasLutea) {
+        this.mediaDiasLutea = mediaDiasLutea;
+    }
 }
--- src/com/fases/FaseOvulacion.java
+++ src/com/fases/FaseOvulacion.java
@@ -1,4 +1,13 @@
 package com.fases;
 
 public class FaseOvulacion {
+    float mediaDiasOvulacion;
+
+    public float getMediaDiasOvulacion() {
+        return mediaDiasOvulacion;
+    }
+
+    public void setMediaDiasOvulacion(float mediaDiasOvulacion) {
+        this.mediaDiasOvulacion = mediaDiasOvulacion;
+    }
 }
