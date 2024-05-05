dfbd2bffd387e953b42538b9df12f23524181b07
Sergio <ssanromangiraldez@danielcastelao.org>
2024-05-05 16:14:15 +0200

    Getters y setters de fecha del periodo

--- src/com/periodo/RegistroFinPeriodo.java
+++ src/com/periodo/RegistroFinPeriodo.java
@@ -1,4 +1,14 @@
 package com.periodo;
 
 public class RegistroFinPeriodo {
+
+    private String fechaFin;
+
+    public String getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(String fechaFin) {
+        this.fechaFin = fechaFin;
+    }
 }
