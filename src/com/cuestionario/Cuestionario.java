58df28ff4a2996cf569d5b5df8655a15cd4767b1
Keyla Iglesias <kiglesiasesteves@danielcastelao.org>
2024-05-08 12:37:40 +0200

    Creacion JAVADOC de Cuestionario

--- src/com/cuestionario/Cuestionario.java
+++ src/com/cuestionario/Cuestionario.java
@@ -1,15 +1,31 @@
 package com.cuestionario;
 
+/**
+ * La clase Cuestionario es una clase singleton que maneja una instancia de CuestionarioSwing y Duracion.
+ * Proporciona métodos para obtener la duración del ciclo y la duración del sangrado, y para imprimir estos valores.
+ *
+ * @author kiglesiasesteves
+ */
 public class Cuestionario {
-    private static Cuestionario instance = null;
-    private CuestionarioSwing cuestionarioGUI;
-    private Duracion duracion;
+    private static Cuestionario instance = null; // La única instancia de Cuestionario
+    private CuestionarioSwing cuestionarioGUI; // La interfaz gráfica de usuario
+    private Duracion duracion; // La duración del ciclo y del sangrado
 
+    /**
+     * Constructor privado de la clase Cuestionario.
+     * Inicializa la duración y la interfaz gráfica de usuario.
+     */
     private Cuestionario() {
         duracion = new Duracion();
         cuestionarioGUI = new CuestionarioSwing(duracion);
     }
 
+    /**
+     * Método para obtener la única instancia de Cuestionario.
+     * Si la instancia no existe, se crea una nueva.
+     *
+     * @return La única instancia de Cuestionario.
+     */
     public static Cuestionario getInstance() {
         if (instance == null) {
             instance = new Cuestionario();
@@ -17,16 +33,29 @@ public class Cuestionario {
         return instance;
     }
 
+    /**
+     * Método para obtener la duración del ciclo.
+     *
+     * @return La duración del ciclo.
+     */
     public int getDuracionCiclo() {
         return  duracion.getDuracionCiclo();
     }
 
+    /**
+     * Método para obtener la duración del sangrado.
+     *
+     * @return La duración del sangrado.
+     */
     public int getDuracionSangrado() {
         return duracion.getDuracionSangrado();
     }
+
+    /**
+     * Método para imprimir la duración del ciclo y la duración del sangrado.
+     */
     public void imprimirDuracionCiclo() {
         System.out.println("Duración del ciclo: " + getDuracionCiclo());
         System.out.println("Duración del sangrado: " + getDuracionSangrado());
-
     }
 }
