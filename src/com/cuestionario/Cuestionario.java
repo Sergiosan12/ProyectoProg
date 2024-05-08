c3084c7104225f3fa00dd18cf3092c04832c434e
Keyla Iglesias <kiglesiasesteves@danielcastelao.org>
2024-05-08 12:24:55 +0200

    Creacion métodos cuestionarios

--- src/com/cuestionario/Cuestionario.java
+++ src/com/cuestionario/Cuestionario.java
@@ -1,4 +1,32 @@
 package com.cuestionario;
 
 public class Cuestionario {
+    private static Cuestionario instance = null;
+    private CuestionarioSwing cuestionarioGUI;
+    private Duracion duracion;
+
+    private Cuestionario() {
+        duracion = new Duracion();
+        cuestionarioGUI = new CuestionarioSwing(duracion);
+    }
+
+    public static Cuestionario getInstance() {
+        if (instance == null) {
+            instance = new Cuestionario();
+        }
+        return instance;
+    }
+
+    public int getDuracionCiclo() {
+        return  duracion.getDuracionCiclo();
+    }
+
+    public int getDuracionSangrado() {
+        return duracion.getDuracionSangrado();
+    }
+    public void imprimirDuracionCiclo() {
+        System.out.println("Duración del ciclo: " + getDuracionCiclo());
+        System.out.println("Duración del sangrado: " + getDuracionSangrado());
+
+    }
 }
