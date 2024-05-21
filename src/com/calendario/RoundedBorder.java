4b21e77ba02a1c8ac75ae393e67441a796e0ad83
Keyla Iglesias <kiglesiasesteves@danielcastelao.org>
2024-05-21 11:01:37 +0200

    Añadir lastperiodo to tabla menstruacion

---
+++ src/com/calendario/RoundedBorder.java
@@ -0,0 +1,30 @@
+package com.calendario;
+
+
+import javax.swing.border.Border;
+import java.awt.*;
+
+// Class for creating rounded borders
+class RoundedBorder implements Border {
+    private int radius;
+
+    RoundedBorder(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return new Insets(radius, radius, radius, radius);
+    }
+
+    @Override
+    public boolean isBorderOpaque() {
+        return true;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        g.setColor(c.getForeground());
+        g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+    }
+}
