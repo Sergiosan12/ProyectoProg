f11e02e6eeaa49df7dbb5184f644e2b84378696f
Keyla Iglesias <kiglesiasesteves@danielcastelao.org>
2024-05-29 10:54:05 +0200

    Refactoring HandlerUsuario

--- src/com/database/DatabaseHandlerUsuario.java
+++ src/com/database/DatabaseHandlerUsuario.java
@@ -7,11 +7,21 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * La clase DatabaseHandlerUsuario maneja la conexión y las consultas
+ * a la base de datos relacionadas con los usuarios.
+ */
 public class DatabaseHandlerUsuario {
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/proyectodb";
     private static final String USER = "postgres";
     private static final String PASS = "debian";
 
+    /**
+     * Recupera los datos de un usuario específico de la base de datos.
+     *
+     * @param usuario El nombre del usuario.
+     * @return Un objeto Usuario con los datos recuperados o null si no se encuentra el usuario.
+     */
     public Usuario selectData(String usuario) {
         Usuario resultado = null;
         Connection conn = null;
@@ -32,11 +42,8 @@ public class DatabaseHandlerUsuario {
 
             // Procesar el conjunto de resultados
             if (rs.next()) {
-                String nombre = rs.getString("nombre");
-                String email = rs.getString("email");
-                String contrasena = rs.getString("contrasenha");
-                int edad = rs.getInt("edad");
-                resultado = new Usuario(nombre, email, contrasena, edad, usuario);
+                Result result = getResult(rs);
+                resultado = new Usuario(result.nombre(), result.email(), result.contrasena(), result.edad(), usuario);
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -59,5 +66,26 @@ public class DatabaseHandlerUsuario {
             }
         }
         return resultado;
+    }
+
+    /**
+     * Procesa el ResultSet para extraer los datos del usuario.
+     *
+     * @param rs El ResultSet que contiene los datos de la consulta.
+     * @return Un objeto Result con los datos extraídos del ResultSet.
+     * @throws SQLException Si ocurre un error al procesar el ResultSet.
+     */
+    private static Result getResult(ResultSet rs) throws SQLException {
+        String nombre = rs.getString("nombre");
+        String email = rs.getString("email");
+        String contrasena = rs.getString("contrasenha");
+        int edad = rs.getInt("edad");
+        return new Result(nombre, email, contrasena, edad);
+    }
+
+    /**
+     * La clase Result es una clase auxiliar que almacena los datos del usuario.
+     */
+    private record Result(String nombre, String email, String contrasena, int edad) {
     }
 }
