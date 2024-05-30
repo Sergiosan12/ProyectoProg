b88a2cb1e799b7c8d92ea9908f69ffffa9957322
Keyla Iglesias <kiglesiasesteves@danielcastelao.org>
2024-05-30 12:59:30 +0200

    Refactor SignIn

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             SignIn signIn = new SignIn();
-            signIn.setSize(1200, 570);
+            signIn.setSize(1200, 560);
             signIn.setVisible(true);
             signIn.setLocationRelativeTo(null);
             signIn.setResizable(false);
