7bd91efa7e74ff298632a3837f4f2d035c04a7d4
Keyla Iglesias <kiglesiasesteves@danielcastelao.org>
2024-05-31 12:21:45 +0200

    Refactoring SelectorFecha

--- src/com/view/cuestionarios/sangrado/SelectorFecha.java
+++ src/com/view/cuestionarios/sangrado/SelectorFecha.java
@@ -18,6 +18,10 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Properties;
 
+/**
+ * La clase SelectorFecha proporciona una interfaz gráfica para seleccionar la fecha del último periodo menstrual.
+ * Permite al usuario seleccionar una fecha y actualizar la información en la base de datos.
+ */
 public class SelectorFecha extends JFrame {
 
     // Constantes de mensajes
@@ -34,15 +38,26 @@ public class SelectorFecha extends JFrame {
     private final InsertDatabaseMenstruacion insertDatabase = new InsertDatabaseMenstruacion();
     private Menstruacion menstruacion;
 
+    /**
+     * Constructor vacío de la clase SelectorFecha.
+     */
     public SelectorFecha() {
         // Constructor vacío si es necesario
     }
 
+    /**
+     * Constructor de la clase SelectorFecha que acepta un objeto Menstruacion.
+     *
+     * @param menstruacion el objeto Menstruacion que contiene la información menstrual del usuario.
+     */
     public SelectorFecha(Menstruacion menstruacion) {
         this.menstruacion = menstruacion;
         initialize();
     }
 
+    /**
+     * Inicializa los componentes de la interfaz gráfica.
+     */
     private void initialize() {
         try {
             JPanel panel = createMainPanel();
@@ -54,6 +69,9 @@ public class SelectorFecha extends JFrame {
         setupFrame();
     }
 
+    /**
+     * Configura la ventana principal.
+     */
     private void setupFrame() {
         setTitle("Selector de Fecha");
         setSize(600, 400);
@@ -62,12 +80,22 @@ public class SelectorFecha extends JFrame {
         setVisible(true);
     }
 
+    /**
+     * Añade el botón "Continuar" al panel principal.
+     *
+     * @param panel el panel principal.
+     */
     private void addContinueButton(JPanel panel) {
         JButton btnContinuar = createContinueButton();
         btnContinuar.addActionListener(e -> handleContinueButton(panel));
         addButtonToPanel(panel, btnContinuar);
     }
 
+    /**
+     * Crea el botón "Continuar" estilizado.
+     *
+     * @return el botón "Continuar" estilizado.
+     */
     private JButton createContinueButton() {
         JButton btnContinuar = new JButton("Continuar");
         btnContinuar.setBackground(new Color(255, 105, 180));
@@ -75,6 +103,12 @@ public class SelectorFecha extends JFrame {
         return btnContinuar;
     }
 
+    /**
+     * Añade el botón estilizado al panel principal.
+     *
+     * @param panel  el panel principal.
+     * @param button el botón a añadir.
+     */
     private void addButtonToPanel(JPanel panel, JButton button) {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
@@ -83,6 +117,11 @@ public class SelectorFecha extends JFrame {
         panel.add(button, gbc);
     }
 
+    /**
+     * Maneja la acción del botón "Continuar", procesando la fecha seleccionada y actualizando la base de datos.
+     *
+     * @param panel el panel principal.
+     */
     private void handleContinueButton(JPanel panel) {
         try {
             Date selectedDate = (Date) datePicker.getModel().getValue();
@@ -99,6 +138,12 @@ public class SelectorFecha extends JFrame {
         }
     }
 
+    /**
+     * Actualiza los datos menstruales con la fecha seleccionada y guarda los cambios en la base de datos.
+     *
+     * @param selectedDate la fecha seleccionada.
+     * @throws Exception si ocurre un error durante el proceso.
+     */
     private void updateMenstruationData(Date selectedDate) throws Exception {
         GenerateDiaFases.CambiarDateLastPeriod(selectedDate);
         menstruacion.setLastperiod(selectedDate);
@@ -106,11 +151,19 @@ public class SelectorFecha extends JFrame {
         insertDatabase.insertDateIntoDatabase(menstruacion);
     }
 
+    /**
+     * Abre la ventana de uso del programa.
+     */
     private void openUsoProg() {
         UsoProg usoProg = new UsoProg(menstruacion);
         usoProg.setVisible(true);
     }
 
+    /**
+     * Crea el panel principal que contiene el selector de fecha.
+     *
+     * @return el panel principal.
+     */
     private JPanel createMainPanel() {
         try {
             setupCalendar();
@@ -125,6 +178,9 @@ public class SelectorFecha extends JFrame {
         }
     }
 
+    /**
+     * Configura el calendario con la fecha actual y la fecha de hace un año.
+     */
     private void setupCalendar() {
         Calendar calendar = Calendar.getInstance();
         today = calendar.getTime();
@@ -132,6 +188,11 @@ public class SelectorFecha extends JFrame {
         oneYearAgo = calendar.getTime();
     }
 
+    /**
+     * Crea un modelo de fecha configurado con la fecha actual y añade un listener para controlar el rango de fechas seleccionables.
+     *
+     * @return el modelo de fecha.
+     */
     private UtilDateModel createDateModel() {
         Calendar calendar = Calendar.getInstance();
         int currentYear = calendar.get(Calendar.YEAR);
@@ -146,6 +207,11 @@ public class SelectorFecha extends JFrame {
         return model;
     }
 
+    /**
+     * Añade un listener al modelo de fecha para controlar el rango de fechas seleccionables.
+     *
+     * @param model el modelo de fecha.
+     */
     private void addDateRangeListener(UtilDateModel model) {
         model.addPropertyChangeListener(evt -> {
             if ("value".equals(evt.getPropertyName())) {
@@ -154,6 +220,12 @@ public class SelectorFecha extends JFrame {
         });
     }
 
+    /**
+     * Maneja el cambio de fecha, asegurándose de que la fecha seleccionada esté dentro del rango válido.
+     *
+     * @param evt   el evento de cambio de propiedad.
+     * @param model el modelo de fecha.
+     */
     private void handleDateChange(PropertyChangeEvent evt, UtilDateModel model) {
         Object selectedValue = evt.getNewValue();
         if (selectedValue instanceof Date) {
@@ -165,6 +237,12 @@ public class SelectorFecha extends JFrame {
         }
     }
 
+    /**
+     * Crea el panel de selección de fecha con las propiedades especificadas.
+     *
+     * @param model el modelo de fecha.
+     * @return el panel de selección de fecha.
+     */
     private JDatePanelImpl createDatePanel(UtilDateModel model) {
         Properties p = new Properties();
         p.put("text.today", "Hoy");
@@ -173,6 +251,12 @@ public class SelectorFecha extends JFrame {
         return new JDatePanelImpl(model);
     }
 
+    /**
+     * Crea un panel con los componentes de selección de fecha.
+     *
+     * @param datePicker el componente de selección de fecha.
+     * @return el panel con los componentes de selección de fecha.
+     */
     private JPanel createPanelWithComponents(JDatePickerImpl datePicker) {
         JPanel panel = new JPanel(new GridBagLayout());
         panel.setBackground(new Color(255, 241, 241));
@@ -191,13 +275,27 @@ public class SelectorFecha extends JFrame {
 
         return panel;
     }
+        /**
+         * Muestra un mensaje emergente.
+         *
+         * @param parent el componente padre del cuadro de diálogo.
+         * @param message el mensaje a mostrar.
+         * @param title el título del cuadro de diálogo.
+         * @param messageType el tipo de mensaje.
+         */
+        private void showMessage (Component parent, String message, String title,int messageType){
+            JOptionPane.showMessageDialog(parent, message, title, messageType);
+        }
 
-    private void showMessage(Component parent, String message, String title, int messageType) {
-        JOptionPane.showMessageDialog(parent, message, title, messageType);
+        /**
+         * Maneja las excepciones mostrando un cuadro de diálogo con el mensaje de error.
+         *
+         * @param message el mensaje de error a mostrar.
+         * @param e la excepción que se ha producido.
+         */
+        private void handleException (String message, Exception e){
+            e.printStackTrace();
+            showMessage(this, message + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
-    private void handleException(String message, Exception e) {
-        e.printStackTrace();
-        showMessage(this, message + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-    }
-}
